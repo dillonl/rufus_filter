1137212b334eb976e3464efff540bd64c09d222d
dillonl <dlee@genetics.utah.edu>
2016-05-24 17:25:11 -0600

    adding initial files

---
+++ tests/gtest_main.cpp
@@ -0,0 +1,11 @@
+#include "gtest/gtest.h"
+
+//#include "config/DataPaths.h"
+//#include "AlignmentParserTests.hpp"
+//#include "SparseKmerSetTests.hpp"
+
+GTEST_API_ int main(int argc, char** argv)
+{
+	::testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
+}
